2e9728219cae0479d26e2426a3a79f51d66f84e2
philkrause <pkrause@exzeo.com>
2025-06-11 19:39:40 -0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/starwars-survivor/' : '/', 
+  base: mode === 'production' ? '/starwars-survivor/' : '/', // ✅ important
   server: {
     host: '127.0.0.1',
     port: 3000,
